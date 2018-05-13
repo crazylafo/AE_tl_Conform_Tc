1877dc78b7e6c21c60168255bf09720a09344837
crazylafo <31342452+crazylafo@users.noreply.github.com>
2018-05-13 18:35:31 +0200

    Add files via upload

--- MediaInfoBat/medialist.bat
+++ MediaInfoBat/medialist.bat
@@ -1,21 +1,20 @@
 @echo off
 
 echo ********** Create txt MediaInfo Files **********
-set "mediainfo_path=.\MediaInfo"
+set "mediainfo_path=C:\Program Files\MediaInfo_CLI\MediaInfo.exe"
 set "output_file=\mediaList.txt" 
 
 
 echo.
 echo Looking for Media Assets on target directory . . .
-  dir %1 *.mxf *.mov  /b /s >> %1\filelist.tmp
-  set "templist = %1\filelist.tmp"
-REM  ********  ********
-(for /f "delims=" %%i in ("%1\!filelist.tmp!") do (
+dir %1\*.mov /s/b/o:n > "%1\filelist.txt"
+(for /F  "usebackq delims=" %%i in ("%1\filelist.txt") do (
+	echo %%i 
     setlocal enabledelayedexpansion
-    "!mediainfo_path!"  -f --Output "%%i" >> "%1!output_file!"
+    "!mediainfo_path!"  -f --Output "%%i" > "%1!output_file!"
     echo()
     endlocal
 )
 
-del "%1\filelist.tmp"
+del "%1\filelist.txt"
 echo.
